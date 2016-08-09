41ca22447e545260c6c3e2d9e32b81cca9eb1d09
hayden <hayden@example.com>
2016-08-09 11:41:07 +1000

    :feelsgood: Entity Framework and WCF Structure

---
+++ FleetServer/FleetService.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FleetServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FleetService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select FleetService.svc or FleetService.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class FleetService : IFleetService
+    {
+        //NOTE(Al): We need to implement this
+    }
+}
---
+++ FleetServer/IFleetService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using FleetTransferObjects;
+
+namespace FleetServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFleetService" in both code and config file together.
+    [ServiceContract]
+    public interface IFleetService
+    {
+        // Registration
+        FleetClientToken RegisterClient();
+
+        //  Heartbeat
+        [OperationContract]
+        FleetHearbeatEnum Heartbeat(FleetClientToken token);
+
+        // Files
+        [OperationContract]
+        List<FleetFileIdentifier> QueryFiles(FleetClientToken token);
+
+        [OperationContract]
+        FleetFile GetFile(FleetClientToken token, FleetFileIdentifier fileId);
+
+        [OperationContract(Name = "SendFileSingleRecipient")]
+        Boolean SendFile(FleetClientToken token, FleetClientIdentifier recipient, FleetFile file);
+
+        [OperationContract(Name = "SendFileMultipleRecipient")]
+        Boolean SendFile(FleetClientToken token, List<FleetClientIdentifier> recipients, FleetFile file);
+    }
+}
